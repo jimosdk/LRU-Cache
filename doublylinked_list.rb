d88c8e023b0c3bcd3b51da973d430c022472ec7e
jimosk <jimosdk@gmail.com>
2021-06-19 21:11:15 +0300

    Initial commit implemented doubly linked list

---
+++ doublylinked_list.rb
@@ -0,0 +1,90 @@
+require 'byebug'
+require_relative 'link'
+class DoublyLinkedList
+    attr_reader :size,:head,:tail
+    def initialize
+        @head = nil
+        @tail = nil
+        @size = 0
+    end
+
+    def add_last(value)
+        if empty?
+            @head = Link.new(value)
+            @tail = @head
+        else  
+            link = Link.new(value,@tail)
+            @tail.next_ptr = link
+            @tail = link
+        end
+        @size += 1
+        @tail
+    end
+
+    def add_first(value)
+        if empty?
+            @head = Link.new(value)
+            @tail = @head
+        else  
+            link = Link.new(value,nil,@head)
+            @head.prev_ptr = link
+            @head = link
+        end
+        @size += 1
+        @head
+    end
+
+    def delete(value)
+        link = find_link(value)
+        return nil if link.nil?
+
+        prev = link.prev_ptr
+        n = link.next_ptr
+
+        prev.next_ptr = n unless @head == link
+        n.prev_ptr = prev unless @tail == link
+
+        @head = n if @head == link 
+        @tail = prev if @tail == link
+
+        @size -= 1
+        link
+    end
+
+    def append_after(value,new_value)
+        link = find_link(value)
+        return nil if link.nil?
+        n = link.next_ptr
+        new_link = Link.new(new_value,link,n)
+        link.next_ptr = new_link
+
+        n.prev_ptr = new_link unless @tail == link
+        @tail = new_link if @tail == link
+
+        @size += 1
+        new_link
+    end
+
+    def empty?
+        @size == 0
+    end
+
+    def find_link(value)
+        ptr = @head
+        until ptr == nil || ptr.value == value
+            ptr = ptr.next_ptr
+        end
+        ptr
+    end
+
+    def print_list
+        ptr = @head
+        list_str = ''
+        until ptr == nil
+            list_str += ptr.value.to_s + "->"
+            ptr = ptr.next_ptr
+        end
+        puts list_str + "nil"
+        nil
+    end
+end
---
+++ link.rb
@@ -0,0 +1,10 @@
+require 'byebug'
+
+class Link
+    attr_accessor :value ,:next_ptr,:prev_ptr
+    def initialize(value,prev = nil,n = nil)
+        @value = value
+        @next_ptr = n
+        @prev_ptr = prev
+    end
+end
